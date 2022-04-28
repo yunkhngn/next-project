07852590efe11bb495823da53fbf46740cb2db4a
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-29 00:37:34 +0700

    Rearrange folder

--- components/Hooks/Metatags/Metatags.js
+++ components/Hooks/Metatags/Metatags.js
@@ -19,19 +19,19 @@ export default function Metatags({description}) {
             <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
             <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
             <meta name="theme-color" content="#000000" />
-            <link rel="apple-touch-icon" sizes="57x57" href="/image/apple-icon-57x57.png" />
-            <link rel="apple-touch-icon" sizes="60x60" href="/image/apple-icon-60x60.png" />
-            <link rel="apple-touch-icon" sizes="72x72" href="/image/apple-icon-72x72.png" />
-            <link rel="apple-touch-icon" sizes="76x76" href="/image/apple-icon-76x76.png" />
-            <link rel="apple-touch-icon" sizes="114x114" href="/image/apple-icon-114x114.png" />
-            <link rel="apple-touch-icon" sizes="120x120" href="/image/apple-icon-120x120.png" />
-            <link rel="apple-touch-icon" sizes="144x144" href="/image/apple-icon-144x144.png" />
-            <link rel="apple-touch-icon" sizes="152x152" href="/image/apple-icon-152x152.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-icon-180x180.png" />
-            <link rel="icon" type="image/png" sizes="192x192" href="/image/android-icon-192x192.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="96x96" href="/image/favicon-96x96.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
             <link rel="manifest" href="/manifest.json" />
             <meta name="msapplication-TileColor" content="#000000" />
             <meta name="msapplication-TileImage" content="/image/ms-icon-144x144.png" />
