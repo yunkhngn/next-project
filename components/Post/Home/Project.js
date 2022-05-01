388e33cfcf50ced20fc48f6853338753687a6488
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-05-02 03:12:32 +0700

    retesting

--- components/Post/Home/Project.js
+++ components/Post/Home/Project.js
@@ -9,26 +9,31 @@ const Project = ({theme,themeUse}) => {
             id: 1,
             title: 'React webapp.',
             date: '12/04/2021',
+            url: '/react-webapp',
         },
         {
             id: 2,
             title: 'Portfolio using html/css/js.',
             date: '06/05/2021',
+            url: '/portfolio-using-html-css-js',
         },
         {
             id: 3,
             title: 'Portfolio using Reactjs.',
             date: '07/12/2021',
+            url: '/portfolio-using-reactjs',
         },
         {
             id: 4,
             title: 'Counting down to my birthday.',
             date: '16/04/2022',
+            url: '/counting-down-to-my-birthday',
         },
         {
             id: 5,
             title: 'My first project using Nextjs.',
             date: '30/04/2022',
+            url: '/my-first-project-using-nextjs',
         },
     ]
     return (
@@ -40,7 +45,7 @@ const Project = ({theme,themeUse}) => {
             <Div>
                 {post.map(item => (
                     <div key={item.id}>
-                        <Link href='/projects'>
+                        <Link href={'/projects/'+item.url}>
                             <a>
                                 <Div justify="space-between" align="center" d="flex" hoverBg={theme === 'light' ? "gray200" : '#222222'} rounded='12px' p="16px" transition m={{r:"-16px", l:"-16px"}}>
                                     <Para margin="true" which="right" color={theme === 'light' ? '#171717' : "#ededed"}><strong>{item.title}</strong></Para>
