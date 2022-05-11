b085e9099f25e1ac13b43f13c84a963d6fc02c7a
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-05-11 08:52:06 +0700

    Updating Project

--- components/Post/Home/Project.js
+++ components/Post/Home/Project.js
@@ -3,60 +3,26 @@ import Link from 'next/link'
 import {Div} from 'atomize'
 import ElementSpace from '../ElementSpace'
 
-const Project = ({theme,themeUse}) => {
-    const post = [
-        {
-            id: 1,
-            title: 'React webapp.',
-            date: '12/04/2021',
-            url: '/react-webapp',
-        },
-        {
-            id: 2,
-            title: 'Portfolio using html/css/js.',
-            date: '06/05/2021',
-            url: '/portfolio-using-html-css-js',
-        },
-        {
-            id: 3,
-            title: 'Portfolio using Reactjs.',
-            date: '07/12/2021',
-            url: '/portfolio-using-reactjs',
-        },
-        {
-            id: 4,
-            title: 'Counting down to my birthday.',
-            date: '16/04/2022',
-            url: '/counting-down-to-my-birthday',
-        },
-        {
-            id: 5,
-            title: 'My first project using Nextjs.',
-            date: '30/04/2022',
-            url: '/my-first-project-using-nextjs',
-        },
-    ]
+const Project = ({theme,themeUse, prj}) => {
     return (
         <article>
             <Para color={themeUse.secondary} >Incredible things built with React, Next.js, and TypeScript.</Para>
             <Div m={{b:'1.7em'}}/>
             <hr className={'hr'+theme}/>
             <Div>
-                {post.map(item => (
+                {prj.map(item => (
                     <div key={item.id}>
-                        <Link href={'/projects'+item.url}>
-                            <a>
-                                <Div justify="space-between" align="center" d="flex" hoverBg={theme === 'light' ? "gray200" : '#222222'} rounded='12px' p="16px" transition m={{r:"-16px", l:"-16px"}}>
-                                    <Para margin="true" which="right" color={theme === 'light' ? '#171717' : "#ededed"}><strong>{item.title}</strong></Para>
-                                    <hr className={'hr'+theme}/>
-                                    <Para margin="true" which="left" color={themeUse.secondary}>{item.date}</Para>
-                                </Div>
-                            </a>
-                        </Link>
+                        <a target="_blank" href={item.attributes.Link}>
+                            <Div justify="space-between" align="center" d="flex" hoverBg={theme === 'light' ? "gray200" : '#222222'} rounded='12px' p="16px" transition m={{r:"-16px", l:"-16px"}}>
+                                <Para margin="true" which="right" color={theme === 'light' ? '#171717' : "#ededed"}><strong>{item.attributes.Title}</strong></Para>
+                                <hr className={'hr'+theme}/>
+                                <Para margin="true" which="left" color={themeUse.secondary}>{item.attributes.Date}</Para>
+                            </Div>
+                        </a>
                     </div>
                 ))}
             </Div>
-            <Link href="/projects"><a><Div m={{t:"1em"}} textColor={themeUse.secondary} hoverTextColor={themeUse.hover} transition>View all my project...</Div></a></Link>
+            <a target="_blank" href="https://github.com/yunkhngn"><Div m={{t:"1em"}} textColor={themeUse.secondary} hoverTextColor={themeUse.hover} transition>View all my project...</Div></a>
             <ElementSpace space="12em"/>
         </article>
     );
--- pages/projects.js
+++ pages/projects.js
@@ -2,13 +2,23 @@ import {Template, Title} from '../components/Template/'
 import {Project} from '../components/Post/'
 import {desc} from '../lib'
 
-const projects = ({themeUse,theme}) => {
+const projects = ({themeUse,theme,prj}) => {
     return (
         <Template description={desc.projects} height="100%">
             <Title color={themeUse.primary}>Projects</Title>
-            <Project themeUse={themeUse} theme={theme}/>
+            <Project prj={prj} themeUse={themeUse} theme={theme}/>
         </Template>
     );
+}
+
+export async function getServerSideProps(){
+    const URL = require('../lib/url')
+    const res = await fetch(`${URL.url}projects/`);
+    const data = await res.json();
+    const prj = data.data;
+    return {
+        props: {prj},
+    }
 }
 
 export default projects;
